d9c27ee357b059cf41344750dbc89547236db9f8
maxthor500 <iraubuntu@gmail.com>
2022-12-17 19:45:41 +0000

    Update style and script

--- js/script.js
+++ js/script.js
@@ -11,8 +11,14 @@ const setSize = (newSize) => {
 const renderGrid = (size) => {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridElement)
+    }
 }
-
-
 
 renderGrid(DEFAULT_SIZE)
